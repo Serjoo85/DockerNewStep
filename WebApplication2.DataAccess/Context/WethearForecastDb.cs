a44b7284c20dc3b4629515772fc30f8421e7f33b
Serjo85 <bobrosebob07@gmail.com>
2023-08-19 07:04:00 +0300

    Db class added

---
+++ WebApplication2.DataAccess/Context/WethearForecastDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace WebApplication2.DataAccess.Context
+{
+    public class WethearForecastDb : DbContext
+    {
+        public virtual DbSet<WeatherForecast> WeatherForecasts { get; set; }
+
+        public WethearForecastDb(DbContextOptions<WethearForecastDb> options) : base(options)
+        {}
+    }
+}
